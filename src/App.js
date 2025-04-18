655b545436134c0c69da568b99bf11d7a26625d5
Archith Billa <arb06071999@gmail.com>
2025-04-18 11:37:40 -0400

    Error page added

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import About from './Pages/About';
 import Services from './Pages/Services';
 import Contact from './Pages/Contact';
 import './App.css';
+import Error from '../src/components/ErrorPage'
+
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Error/>
         
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
-        </Routes>
+        </Routes> */}
 
         <Footer />
       </div>
